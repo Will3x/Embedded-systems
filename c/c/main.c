277d115129beb4b5582abd82ec9a166f02dca0dd
123wouter <wouter.wouter@hotmail.com>
2018-10-31 11:15:52 +0100

    c project folder toegevoegd.

---
+++ c/c/main.c
@@ -0,0 +1,18 @@
+/*
+ * c.c
+ *
+ * Created: 31-10-2018 11:14:05
+ * Author : Wouter
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
